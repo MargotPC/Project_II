f55281e61192d834e033000c24a2f84647f73f77
Albert2424 <albertixortega@gmail.com>
2023-03-25 18:48:07 +0100

    PARALLEL CODE

---
+++ PARALLEL/README.md
@@ -0,0 +1,49 @@
+# SERIAL CODE
+
+This directory contains all the necessary codes to perform a VdW simulation of a Xe gas in serial. 
+
+## TABLE OF CONTENTS
+
+1. [ REQUIREMENTS ](#1-req)
+2. [ EXECUTION](#2-ex)
+3. [ RESULTS](#3-res)
+
+<a name="1-req"></a>
+### REQUIREMENTS
+
+All codes used for the simulation have been created using _Fortran 90_. The visualization code `Block_average_and_plots.py` is in _python (3.11.2)_. In order to execute the code, the _[gfortran]_ compiler is needed. If you use a different compiler, change the `FC` varaible in the `Makefile`.
+
+In order to visualize the trajectories of the simulation as well as the initial configuration we used [VMD] but any visualization program that can read `.xyz` files can be used.
+
+[gfortran]: https://fortran-lang.org/en/learn/os_setup/install_gfortran/
+[VMD]: https://www.ks.uiuc.edu/Development/Download/download.cgi?PackageName=VMD
+
+<a name="2-ex"></a>
+### EXECUTION
+
+To execute the program in your machine use `make` to compile the code and then use `make run` to start the simulation. If you want to plot your results, use `make plot` after you have runned your simulation. This will save all the plots in `.pdf` format. Notice that when you run the code the directories `/results` and `/plots` are automatically generated. As their names say, all the results generated from the simulation will go to the directory `/results` while all plots will go to `/plots`. 
+
+Before running your simulation, you may want to change the initial conditions for the system configuration. In order to do it, you should change the values of every varaible in the `input.dat` file. Notice that a file named `get_input.dat` is also generated but it is an input file for the ploting code and should not be changed if you want to plot the last generated data.
+
+Use `make help` to get aditional information about the options of the make file.
+
+<a name="3-res"></a>
+### RESULTS
+After running your simulation with the default input file your `/results` directory should contain the following files:
+
+```Markdown
+
+125_dynamics_0.800.xyz    (contains the frames of the simulation in .xyz format)
+125_energy_0.800.dat      (Contains in this order time,potential energy,kinetic energy,total energy,instant temperature,momentum,pressure and msd of every frame)
+125_gdr_0.800.dat         (Contains the GDR and distances)
+125_msd_0.800.dat         (Contains time and MSD)
+thermo_prop.dat           (Contains the average values of all the thermodynamic properties)
+initial_conf_0.800.xyz    (Contains the frame of the starting configuration)
+
+```
+
+Notice that the files start with the number of particles of the system $M^3$ (125) and end with the value of the `density` (0.800) of the `input.dat` file respectively. 
+
+After running `Block_average_and_plots.py`, the `/plots` directory may contain all the plots required. It is important that the `/results` directory contains all the specified files!
+
+
---
+++ PARALLEL/main.f90
@@ -0,0 +1,257 @@
+program Molecular_dynamics
+
+use iso_c_binding
+use init_pbc
+use module_forces
+use integrators
+use properties
+use mpi
+
+
+implicit none
+
+integer M,npar,i,n,j,seed,steps,limit,nbins
+real*8 :: density,cutoff,upot,cutoff4,cutoff6,cutoff12,cutoff2,L,dt,E,E_tot,mom,eps,mass,sig,msd
+real*8,allocatable, dimension(:,:) :: pos,lj_force,vel,initial_pos
+real*8,allocatable, dimension(:) :: gdR, distances_gdR
+character(64) :: filename
+character(8) :: fmt,ext
+integer,dimension(3) :: M_values
+real*8 :: sigma,x1,x2,xout1,xout2,T,nu,t_i,P,time,T1,T2
+logical pb
+
+! Initialize MPI
+call MPI_Init(ierr)
+comm = MPI_COMM_WORLD
+call MPI_Comm_rank(comm, myid, ierr)
+call MPI_Comm_size(comm, nprocs, ierr)
+
+
+seed=165432156
+call srand(seed)
+
+!Get the input from the input file
+call get_command_argument(1, filename)
+
+open(101,file=trim(filename))
+    read(101,*) ! INITIAL CONFIGURATION SC
+    read(101,*) M !size of the system (npar=M**3)
+    read(101,*) mass !in g/mol
+    read(101,*) sig !in Armstrongs (lj parameter)
+    read(101,*) eps !in J (lj parameter)
+    read(101,*) density !in m/sig^3
+
+    read(101,*) ! INITIAL CONDITIONS
+    read(101,*) vel !initial velocity of all the particles
+    read(101,*) nu !probability of accepting a change with thermal andersen
+    read(101,*) dt !timestep
+    read(101,*) T1 !initial temperature of the system so it is disordered
+    read(101,*) T2 !temperature around which the system will be equilibrated
+    read(101,*) steps !steps of the simulation
+
+    read(101,*) ! GDR
+    read(101,*) nbins ! Number of points calculated in the gdR function
+close(101)
+
+
+! ####################################################
+!                INITIAL CONFIGURATION SC
+! ####################################################
+
+
+fmt='(f5.3)'
+
+npar=M**3 ! particles
+pb=.True. !pbc conditions are applied
+
+write(ext,fmt) density
+
+filename='results/initial_conf_'//trim(ext)//'_sc'
+filename=trim(filename)
+
+allocate(pos(npar,3),lj_force(npar,3),vel(npar,3),initial_pos(npar,3))
+
+! ####################################################
+!                  INITIAL CONDITIONS
+! ####################################################
+
+fmt='(f6.4)'
+sigma=sqrt(T1)
+
+! call bimodal(vel,sigma,npar)!set the initial velocities as a bimodal distribution
+
+limit=steps/5000 !write 5000 data
+L=(npar/density)**(1.d0/3.d0) !box length
+cutoff=L/3.d0 !cutoff is set to a third of the box lenght
+cutoff2=cutoff*cutoff
+cutoff4=cutoff2*cutoff2
+cutoff6=cutoff4*cutoff2
+cutoff12=cutoff6*cutoff6
+! nbins = 250 ! Number of points calculated in the gdR function
+allocate(gdR(nbins), distances_gdR(nbins))
+
+call init_scc(npar,3,L,pos,filename) !initial configuration could be generated if needed
+! call read_xyz(filename,npar,pos) !read the initial configuration from xyz file
+initial_pos = pos ! assign the initial positions to save it for the msd calculation
+
+! write(ext,fmt) dt
+
+! ext=trim(ext)
+if (myid.eq.0) then
+
+    open(100, file='results/125_dynamics_'//trim(ext)//'.xyz')
+    open(101, file='results/125_energy_'//trim(ext)//'.dat')
+    open(103, file='results/125_gdr_'//trim(ext)//'.dat')
+    open(102, file='get_input.dat')
+
+! ####################################################
+!                       DINÀMICA
+! ####################################################
+
+
+    write(*,*)''
+    write(*,'(x,A,x,I3)') 'Number of particles:', npar
+    write(*,'(x,A,x,f7.3)') 'Density of the system:',density
+    write(*,'(x,A,x,f7.3,A,x,f7.3)') 'Thermostat temperature:',T1,' --->',T2 
+    write(*,'(x,A,x,f7.3)') 'Box length:',L
+    write(*,*) 'Andersen Thermostat: ON'
+    write(*,'(x,A,x,I3)') 'Number of processors:',nprocs
+    print*,''
+
+    write(*,*) 'Output files generated into /data:'
+    write(*,'(8x,A)') '125_dynamics_'//trim(ext)//'.xyz'
+    write(*,'(8x,A)') '125_energy_'//trim(ext)//'.dat'
+    write(*,'(8x,A)') 'get_input.dat'
+    write(102,'(A)') 'INPUT FILE'
+    write(102,'(A)') 'results/125_energy_'//trim(ext)//'.dat'
+    ! write(*,'(8x,A)') 'initial_vel.dat'
+    ! write(*,'(8x,A)') 'final_vel.dat'
+    write(*,'(8x,A)') 'initial_conf_'//trim(ext)//'_sc.xyz'
+    write(*,'(8x,A)') '125_gdr_'//trim(ext)//'.dat'
+    write(102,'(A)') 'results/125_gdr_'//trim(ext)//'.dat'
+    write(102,'(A)') '1000'
+    write(102,'(A)') ''
+    write(102,'(A)') 'THIS INPUT FILE READS THE NAME OF THE OUTPUT FILES AND THE NUMBER OF STEPS NEEDED TO EQUILIBRATE THE SYSTEM'
+
+    print*, ''
+    write(*,*) '~~~~ STARTING MOLECULAR DYNAMICS ~~~~'
+
+    write(100,*) npar
+    write(100,*) ''
+
+    do j=1,npar!writes the initial configuration.
+
+        write(100,*) 'Xe',pos(j,:)
+
+    enddo
+
+    write(100,*) ''
+    write(100,*) ''
+
+    write(101,*) '# Density in g/cm^3:',density*mass/(6.022d0*1d23*(sig*1d-8)**3)
+
+endif
+
+call calc_radial_dist_func(npar, density, nbins, pos, gdR,distances_gdR, 1)
+
+T=T1
+do i=0,steps
+
+    if (i.eq.1d4) then!condition for, when then system is disordered, 
+                      !starting the real simulation.
+        T=T2 
+        sigma=sqrt(T)
+    endif
+
+    if ((mod(i,limit)==0).and.(myid.eq.0)) then
+
+        do j=1,npar
+
+            write(100,*) 'Xe',pos(j,:)
+
+        enddo
+
+        write(100,*) ''
+        write(100,*) ''
+    endif
+
+    ! if (i.eq.10000) then
+    !     sigma=sqrt(2.d0)
+    ! endif
+    
+    call time_step_v_verlet(pos,vel,L,dt,npar,cutoff) !activate use verlet steps
+    ! call time_step_Euler_pbc(pos,L,dt,npar,cutoff,vel) !activate to use euler steps
+    call therm_Andersen(vel,nu,sigma,npar) !activate to add a thermostat
+
+    call pbc2(pos,L,npar)
+
+
+
+
+    if (mod(i,limit)==0) then
+
+        call calc_kinetic_energy_momentum(npar,vel,E,mom)
+        ! call calc_kinetic_energy(npar, vel, E)
+        call calc_Tinstant(npar,E,t_i)
+        ! call lj_f(cutoff2,cutoff6,cutoff12,npar,pos,L,pb,Upot,lj_force)
+        call LENNARD_JONNES_FORCES(pos, L, cutoff, npar, Upot, lj_force)
+        call calc_pressure(npar,density,t_i,pos,lj_force,L,P)
+        call calc_msd(npar,density,initial_pos,pos,msd)
+        call calc_radial_dist_func(npar, density, nbins, pos, gdR, distances_gdR, 2)
+        
+        E_tot=(E+Upot)*eps !so it is in kJ/mol
+        t_i=eps*1d3/(6.022d0*1.380649d0)*t_i !so it is in K
+        P=eps*1d3/(6.022d0*1d23*(sig*1d-10)**3)*P !so it is in Pa
+        time=sqrt(mass*1d-3/(eps*1d3))*sig*1d-10*i*dt*1d12 !so it is in ps
+        msd=msd*sig*sig    !so it is in Armstrongs
+
+        if (myid.eq.0) then
+            write(101,*) time,Upot*eps,E*eps,E_tot,t_i,mom,P,msd
+        endif
+
+    endif
+
+    if ((mod(i,limit)==0).and.(myid.eq.0)) then
+
+        ! write(*,'(I6,2x,A,1x,I6)',advance='no') i,'out of',steps
+        ! write(*,'(A)',advance='no') repeat(c_backspace,100)
+
+        call progress_bar(i,steps)
+        
+    endif
+
+enddo
+
+call calc_radial_dist_func(npar, density, nbins, pos, gdR,distances_gdR, 3)
+
+if (myid.eq.0) then
+    write(103,*) "#gdR", "#distances"
+    do i=0,nbins
+        write(103,*) gdR(i), distances_gdR(i)*sig*sig
+    enddo
+
+    
+    write(*,*) ""
+
+    close(100)
+    close(101)
+    close(103)
+
+endif
+
+! Uncomment to write a file with the final velocities in order to do a distribution
+! open(100, file='final_vel.dat')
+! do i=1,npar
+
+!     write(100,*) vel(i,:)
+
+! enddo
+
+! close(100)
+
+! Finalize MPI
+call MPI_Finalize(ierr)
+
+
+
+end program
